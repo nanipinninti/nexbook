843a6af4f6d324fd2ff4328b4a99e468eff21347
Nani <nanipinninti123@gmail.com>
2024-12-14 17:20:08 +0530

    Login component task begin

---
+++ src/components/Login/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './index.css'
+const Login = ()=>{
+    return(
+        "Hello"
+    )
+}
+export default Login;
---
+++ src/components/z/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './index.css'
+const z = ()=>{
+    return(
+        "Hello"
+    )
+}
+export default z;

e430384d4313e82eb708d3dd15c34e47a0348b4e
aparnaavanapu <aparnaavanapu8@gmail.com>
2024-12-14 20:07:59 +0530

    sign-up first-up

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -16,11 +16,11 @@ const Signup = ()=>{
                 
                 <form className="sign-up-form-container">
                  <p className="sign-up-heading">Registration</p>
-                 <input type="text" placeholder="Name" onChange={(event) => setName(event.target.value)} className="sign-up-input"/>
-                 <input type="email" placeholder="Email" onChange={(event)=>setEmail(event.target.value)} className="sign-up-input" />
-                 <input type="number" placeholder="Contact Number" onChange={(event)=>setNumber(event.target.value)} className="sign-up-input"/>
-                 <input type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)} className="sign-up-input"/>
-                 <input type="password" placeholder="Confirm Password" onChange={(event) => setNewPassword(event.target.value)} className="sign-up-input"/>
+                 <input type="text" value={name} placeholder="Name" onChange={(event) => setName(event.target.value)} className="sign-up-input"/>
+                 <input type="email"  value={email} placeholder="Email" onChange={(event)=>setEmail(event.target.value)} className="sign-up-input" />
+                 <input type="number" value={number} placeholder="Contact Number" onChange={(event)=>setNumber(event.target.value)} className="sign-up-input"/>
+                 <input type="password" value={password} placeholder="Password" onChange={(event) => setPassword(event.target.value)} className="sign-up-input"/>
+                 <input type="password" value={newPassword} placeholder="Confirm Password" onChange={(event) => setNewPassword(event.target.value)} className="sign-up-input"/>
                  
                  <button type="submit" className="sign-up-btn">Sign up</button>
                  <p className="sign-up-description">Already have an account?<span> Login</span></p>

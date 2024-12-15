5135a6093f335d76ea14b2793c5dd13ae0773129
Nani <nanipinninti123@gmail.com>
2024-12-15 11:52:49 +0530

    ..

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import './index.css'
@@ -12,19 +11,6 @@ const Signup = ()=>{
        const [newPassword,setNewPassword]=useState('')
        const [submittedOnce,setSubmittedOnce] = useState(false)
        const [errorMsg,setErrorMsg] = useState('')
-=======
-import { useState } from "react";
-
-import './index.css'
-const Signup = ()=>{
-    const [name,setName]=useState('')
-    const [email,setEmail]=useState('')
-    const [number,setNumber]=useState('')
-    const [password,setPassword]=useState('')
-    const [newPassword,setNewPassword]=useState('')
-    
-
->>>>>>> 99091c3d0afbdf9e7657e181ad59edda3a9999d0
 
        const check = ()=>{
               // Let's check user name

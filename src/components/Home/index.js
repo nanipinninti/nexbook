24f8939a3015e025110e03888772fffca1c3d448
Balaji <chokkarabalaji@gmail.com>
2024-12-18 18:57:45 +0530

    HotelCard work done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import { useState } from "react";
 import './index.css'
 import Header from "../Header";
+import HotelCard from "../HotelCard";
 const Home = ()=>{
     return(
         <div className="home-container">
             <Header/>
+            <HotelCard/>
             
         </div>
         

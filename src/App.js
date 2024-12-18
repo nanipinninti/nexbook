9366486ecd34f697eb83ffb987dcdc2b2c1ef240
aparnaavanapu <aparnaavanapu8@gmail.com>
2024-12-18 22:48:25 +0530

    footer done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Home from './components/Home';
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="body">
@@ -13,6 +14,8 @@ function App() {
           <Route path='/' element = {<Home />} />
           <Route path='/signup' element = {<Signup />}/>
           <Route path='/login' element = {<Login />}/>
+          <Route path='/footer' element={<Footer/>}/>
+
         </Routes>
       </BrowserRouter>
     </div>

7bff13e4a349ffe76327ec9aff07c2eadcaa5e84
Nani <nanipinninti123@gmail.com>
2024-12-18 18:16:41 +0530

    assigning hotel card to balaji

---
+++ src/components/HotelCard/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+import './index.css'
+
+const data =   {
+    "name": "Grand Palm Resort",
+    "price": 20000,
+    "city": "Visakhapatnam",
+    "address": "Visakhapatnam, Andhra Pradesh",
+    "description": "A luxurious escape with breathtaking ocean views.",
+    "image_url": "https://i.postimg.cc/wBXSCMzj/1.jpg",
+    "capacity": 300
+  }
+const HotelCard = ()=>{
+    return(
+        ""
+    )
+}
+export default HotelCard;

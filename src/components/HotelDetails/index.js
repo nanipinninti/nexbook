82d1ff61754f4933119f0f0a8854c5386710018f
Nani <nanipinninti123@gmail.com>
2024-12-25 12:29:22 +0530

    ..

--- src/components/HotelDetails/index.js
+++ src/components/HotelDetails/index.js
@@ -8,20 +8,43 @@ import './index.css'
 import { CiHeart } from "react-icons/ci";
 import { CiShare2 } from "react-icons/ci";
 import { MdLocationOn } from "react-icons/md";
+import Loading from "../Loading";
 
-const data =   {
-    "name": "Grand Palm Resort",
-    "price": 20000,
-    "city": "Visakhapatnam",
-    "address": "Visakhapatnam, Andhra Pradesh",
-    "description": "A luxurious escape with breathtaking ocean views.",
-    "image_url": "https://i.postimg.cc/wBXSCMzj/1.jpg",
-    "capacity": 300
-  }
+const names = {
+    loading : "Loading",
+    active : "active",
+    failure : "failure"
+}
+const HotelDetails = ()=> {
+    const [data, setData] = useState({});
+    const [status, setStatus] = useState(names.loading);
+    const { hotel_id } = useParams();
 
-const HotelDetails = ()=>{
-    const {name,price,city,address,description,image_url,capacity} = data
-    return(
+    useEffect(() => {
+        fetchDetails();
+    }, [hotel_id]);
+
+    const fetchDetails = async () => {
+        const api = `http://localhost:5001/function-halls/queries/hotel-details?hotel_id=${hotel_id}`;
+        const options = {
+            method: "GET"
+        };
+        try {
+            const response = await fetch(api, options);
+            if (response.ok) {
+                const Data = await response.json();
+                setData(Data.data);
+                setStatus(names.active);
+            } else {
+                setStatus(names.failure);
+            }
+        } catch (error) {
+            setStatus(names.failure);
+        }
+    }
+
+    const { name, price, city, address, description, image_url, capacity } = data;
+    return (
         <div className="hotel-details-container">
             <Header />
             <Options />
@@ -34,21 +57,27 @@ const HotelDetails = ()=>{
                         <p className="hall-name">{name}</p>
                         <p className="hall-price">{price}/-</p>
                     </div>
-                    <div className="hotel-details-address-continer">
+                    <div className="hotel-details-address-container">
                         <MdLocationOn className="hotel-details-icon-location" />
                         <p className="hotel-details-address">{address}</p>
                     </div>
                     
                     <p className="hall-description">{description}</p>
+                    <span> Capacity: 1000</span>
                     <div className="hall-details-icons-container">
-                        <div className="likes-continer">
+                        <div className="likes-container">
                         <CiHeart className="hotel-details-icon" />
-                        <p className="hotel-card-noof-likes">0</p>
+                        <p className="hotel-details-noof-likes">0</p>
                         </div>
-                        <div className="shares-continer">
+                        <div className="shares-container">
                         <CiShare2 className="hotel-details-icon" />
-                        <p className="hotel-card-noof-likes">0</p>
+                        <p className="hotel-details-noof-likes">0</p>
                         </div>
+                        <div className="date-input-container">
+                          <i className="calendar-icon"></i>
+                          <input type="date" className="hotel-details-date-input" />
+                        </div>
+
                     </div>
                     <div>
                         <button className="hotel-details-btn">View contact</button>
@@ -66,3 +95,5 @@ const HotelDetails = ()=>{
     )
 }
 export default HotelDetails;
+
+
